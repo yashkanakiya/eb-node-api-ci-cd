caba0b97ee3823f66d67801742a9c2e29a1e242d
yashkanakiya <yashkanakiya281297@gmail.com>
2025-11-17 11:47:56 +0530

    Initialize Node.js application with Express, buildspec, and configuration files

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello from Elastic Beanstalk CI/CD!" });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
